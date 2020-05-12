1cb74431266e861ccfac4ca8dd7e68cbe9862e1a
Jonny Carter <jonnycarter@Jonnys-MacBook-Pro.local>
2020-05-12 10:05:45 +0100

    Updates

---
+++ precache-manifest.b6b91e2af0d6dc68cb5b959b008e0226.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cacc840531a0764365807c354b5273af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93cf97bedb406132506d",
+    "url": "/static/css/main.8e369628.chunk.css"
+  },
+  {
+    "revision": "d2a85216fca886290cb6",
+    "url": "/static/js/2.ec17165b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ec17165b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "93cf97bedb406132506d",
+    "url": "/static/js/main.d5f9cf30.chunk.js"
+  },
+  {
+    "revision": "c9913191e565de632244",
+    "url": "/static/js/runtime-main.170f8412.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "minimatrix.github.io/precache-manifest.99e7177ce603273aa7c01e9faeb52dc8.js"
+  "/precache-manifest.b6b91e2af0d6dc68cb5b959b008e0226.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("minimatrix.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
